dd9b083a7ce85186d1c4e788fd6cc4505046e0b3
Bjorn Warland <bwarland@gmail.com>
2021-07-16 17:04:16 +0100

    kbell->sky

--- Python/Pandas/learning-pandas-1.py
+++ Python/Pandas/learning-pandas-1.py
@@ -46,3 +46,28 @@ george_dupe.append(pd.Series({'1974':9}))
 # iat[] eller at[]
 # george_dupe.set_value('1974',9)
 george_dupe.at['1974']=9
+
+george=pd.Series([10,7,22],
+                 index=['1968','1969','1970'],
+                 name='George songs')
+
+# george_dupe.index.is_unique
+nett1=george>10
+nett2=george>7
+nett3=george<22
+nett4=george>22
+
+songs_66=pd.Series([3,None,11,9],
+                   index=['George','Ringo','John','Paul'],
+                   name='Counts')
+
+songs_69=pd.Series([18,22,7,5],
+                   index=['John','Paul','George','Ringo'],
+                   name='Counts')
+
+# for idx, value in songs_66.iteritems(): print(idx,value)
+# for idx in songs_66.keys(): print(idx)
+# songs_66+songs_69
+# songs_66.fillna(0)+songs_69.fillna(0)
+
+
