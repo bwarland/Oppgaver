71fec7561a97be16f1d0718689da318091e73f91
Bjorn Warland <bwarland@gmail.com>
2021-08-31 19:00:09 +0100

    mbell->kbell

--- Python/Think-Python/ch13.py
+++ Python/Think-Python/ch13.py
@@ -4,16 +4,21 @@ import string
 # ,----
 # | ,----
 # | | oppgave 13.1: program som leser en fil og deler opp linjer til ord, og tar vekk såkalt whitespace og punktsetting
-# | `----
+# | `------------------------------------------------------------------------------------------------------------------
 # `----
-
 
 # ,----
 # | ,----
 # | | oppgave 13.2: ta en bok fra Project Gutenberg
-# | `----
+# | `----------------------------------------------
 # `----
 
+# ,----
+# | word_histogram: document -> dictionary
+# `--- -------------------------------------
+# takes a document and returns a histogram (dictionary) with word counts
 
-
-
+# ,----
+# | sentence_length: document -> dictionary
+# `-----------------------------------------
+# takes a document and returns a histogram (dictionary) with sentence length (number of words)
