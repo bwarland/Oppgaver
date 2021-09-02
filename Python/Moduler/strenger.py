173b03df1675d0a1369cdfb0741c19ca389b3012
Bjorn Warland <bwarland@gmail.com>
2021-09-02 16:54:49 +0100

    kbell->sky

--- Python/Moduler/strenger.py
+++ Python/Moduler/strenger.py
@@ -13,3 +13,19 @@ import string
 # '{0},{1},{2}'.format('a','b','c')
 # '{0}{1}{0}'.format('abra','cad') # => 'abracadabra'
 # '{0},{1},{0}'.format('a','b','c') # => 'a,b,a'
+
+eks1="Monthy Python's Flying Cirus"
+abra="abra cadabra"
+
+# print(eks1.upper())
+# print(eks1.lower())
+# print(eks1.split()) # => skriver ut til liste
+# print(eks1.replace("Python","Ananconda"))
+
+# print(string.capwords(abra))
+
+# formatter=string.Formatter()
+
+# print(formatter.format('{website}',website='JournalDev'))
+# print(formatter.format('{} {website}','Welcome to',website='JournalDev'))
+# print('{} {website}'.format('Welcome to',website='JournalDev'))
