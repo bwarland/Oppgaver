dade1f12730539ce1f2dd60f0a3ef161cba155ac
Bjorn Warland <bwarland@gmail.com>
2022-05-19 15:40:06 +0100

    kbell>elisp-tutorial

--- Lisp/Elisp/intro_elisp.el
+++ Lisp/Elisp/intro_elisp.el
@@ -1,11 +1,25 @@
 ;; introduksjon til Elisp
 
 (defun multiply-by-seven (number)
-  "simple example"
-  (interactive "p")
+  "eksempel på funksjonsdefinisjon
+   anger et tall med syv"
+  (interactive "p")  ; "p" står for prefix argument
   (message "the result is %d" (* 7 number)))
 
 ;; men denne trenger et prefiksargument om man påkaller funksjonen fra M-x
 
 (multiply-by-seven 7)
 
+(let ((zebra "stripes") ;; varlist
+      (tiger "fierce")) 
+  (message "One kind of animal has %s and another is %s."
+	   zebra tiger)) ;; body
+
+(defun type-of-animal (characteristics)
+  "determines which characteristics"
+  (if (equal characteristics "fierce")
+      (message "It is a tiger")
+    (message "It is not fierce")))
+
+(type-of-animal "fierce")
+(type-of-animal "striped")
